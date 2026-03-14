1108fdd728a4e2b1d30853a7bace159c1579a162
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-03-14 13:11:56 +0000

    add cluster public dns zone

---
+++ cloud/dns.tf
@@ -0,0 +1,8 @@
+module "dns" {
+  source  = "Azure/avm-res-network-dnszone/azurerm"
+  version = "0.2.1"
+
+  name                = var.domain_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  enable_telemetry    = false
+}
--- cloud/variables.tf
+++ cloud/variables.tf
@@ -8,3 +8,8 @@ variable "admin_group_id" {
   type        = string
   description = "Microsoft Entra ID group to provide AKS admin access to."
 }
+
+variable "domain_name" {
+  type        = string
+  description = "Domain name to use for the cluster public DNS Zone."
+}
