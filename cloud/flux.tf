ea61579238936df17d2354a5ffc8ce3e225da043
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-03-14 12:35:03 +0000

    add resource provider registration dependency to flux extension

--- cloud/flux.tf
+++ cloud/flux.tf
@@ -2,6 +2,11 @@ resource "azurerm_kubernetes_cluster_extension" "flux" {
   name           = "flux"
   cluster_id     = module.aks.resource_id
   extension_type = "microsoft.flux"
+
+  depends_on = [
+    azurerm_resource_provider_registration.kubernetes_configuration,
+    azurerm_resource_provider_registration.kubernetes
+  ]
 }
 
 resource "azurerm_kubernetes_flux_configuration" "apps" {
@@ -17,8 +22,10 @@ resource "azurerm_kubernetes_flux_configuration" "apps" {
   }
 
   kustomizations {
-    name = "flux-system"
-    path = "./apps/development"
+    name                       = "flux-system"
+    path                       = "./apps/development"
+    recreating_enabled         = true
+    garbage_collection_enabled = true
   }
 
   depends_on = [azurerm_kubernetes_cluster_extension.flux]
