ad0116b2e2223b5a4d208660d44f8b94b58780cf
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-03-14 18:32:54 +0000

    add dns contributor role assignment to ingress identity

--- cloud/dns.tf
+++ cloud/dns.tf
@@ -8,3 +8,32 @@ module "dns" {
   resource_group_name = data.azurerm_resource_group.rg.name
   enable_telemetry    = false
 }
+
+module "dns_role_assignment" {
+  source  = "Azure/avm-res-authorization-roleassignment/azurerm"
+  version = "0.3.0"
+
+  enable_telemetry = false
+  role_definitions = {
+    dns_contributor = {
+      name = "DNS Zone Contributor"
+    }
+  }
+  user_assigned_managed_identities_by_principal_id = {
+    aks_ingress_identity = module.aks.ingress_profile_web_app_routing_identity.objectId
+  }
+  role_assignments_for_resources = {
+    dns_zone = {
+      resource_name       = var.domain_name
+      resource_group_name = data.azurerm_resource_group.rg.name
+      role_assignments = {
+        dns_contributor = {
+          role_definition = "dns_contributor"
+          user_assigned_managed_identities = [
+            "aks_ingress_identity"
+          ]
+        }
+      }
+    }
+  }
+}
