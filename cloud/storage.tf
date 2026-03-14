5e391668051771e7031b549432ac388dcb903e4d
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-03-14 13:48:28 +0000

    add storage account for volume storage

---
+++ cloud/storage.tf
@@ -0,0 +1,25 @@
+module "storage" {
+  source  = "Azure/avm-res-storage-storageaccount/azurerm"
+  version = "0.6.7"
+
+  name                            = module.naming.storage_account.name
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  account_kind                    = "StorageV2"
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  default_to_oauth_authentication = true
+  local_user_enabled              = false
+  https_traffic_only_enabled      = true
+  enable_telemetry                = false
+  lock = {
+    kind = "CanNotDelete"
+  }
+  role_assignments = {
+    kubelet = {
+      principal_id               = module.aks.kubelet_identity.objectId
+      role_definition_id_or_name = "Storage Blob Data Contributor"
+      principal_type             = "ServicePrincipal"
+    }
+  }
+}
