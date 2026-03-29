59af15e7c658b97300e3a715a727f4df46969d5a
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-03-29 14:46:31 +0000

    disable public kv access, add private endpoints to admin and app vaults

--- cloud/vault.tf
+++ cloud/vault.tf
@@ -1,3 +1,12 @@
+locals {
+  vault_private_endpoints = {
+    primary = {
+      private_dns_zone_resource_ids = [module.vault_dns.resource_id]
+      subnet_resource_id            = module.aks_vnet.subnets["private_endpoints"].resource_id
+    }
+  }
+}
+
 module "admin_vault" {
   source  = "Azure/avm-res-keyvault-vault/azurerm"
   version = "~> 0.10.2"
@@ -8,15 +17,22 @@ module "admin_vault" {
   name                          = module.admin_naming.key_vault.name_unique
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = "standard"
-  public_network_access_enabled = true
+  public_network_access_enabled = false
   network_acls = {
-    default_action = "Allow"
+    default_action = "Deny"
   }
+
+  private_endpoints = local.vault_private_endpoints
+
 
   role_assignments = {
     kv_admin = {
       principal_id               = data.azurerm_client_config.current.object_id
       role_definition_id_or_name = "Key Vault Administrator"
+    }
+    admin_kv_secrets_officer = {
+      principal_id               = module.admin_identity.principal_id
+      role_definition_id_or_name = "Key Vault Secrets Officer"
     }
   }
 }
@@ -31,10 +47,12 @@ module "vault" {
   name                          = module.naming.key_vault.name_unique
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = "standard"
-  public_network_access_enabled = true
+  public_network_access_enabled = false
   network_acls = {
-    default_action = "Allow"
+    default_action = "Deny"
   }
+
+  private_endpoints = local.vault_private_endpoints
 
   role_assignments = {
     kv_admin = {
@@ -44,6 +62,10 @@ module "vault" {
     aks_kv_secrets_provider = {
       principal_id               = module.aks.key_vault_secrets_provider_identity.objectId
       role_definition_id_or_name = "Key Vault Secrets User"
+    }
+    admin_kv_secrets_officer = {
+      principal_id               = module.admin_identity.principal_id
+      role_definition_id_or_name = "Key Vault Secrets Officer"
     }
   }
 
