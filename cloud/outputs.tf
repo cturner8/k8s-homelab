05d2cb0c3dcb2fd67152be949c0baa17d8a47e47
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-03-29 12:15:03 +0000

    add admin identity outputs

--- cloud/outputs.tf
+++ cloud/outputs.tf
@@ -9,3 +9,11 @@ output "cert_manager_identity_client_id" {
 output "oauth_proxy_identity_client_id" {
   value = module.oauth_proxy_identity.client_id
 }
+
+output "admin_identity_client_id" {
+  value = module.admin_identity.client_id
+}
+
+output "admin_identity_resource_id" {
+  value = module.admin_identity.resource_id
+}
